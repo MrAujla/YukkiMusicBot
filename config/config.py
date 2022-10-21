9f9a2af9d7dedda6341f0ba02cd346afeb16d85d
MrAujla <116376032+MrAujla@users.noreply.github.com>
2022-10-21 22:44:54 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -41,7 +41,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AujlaMusicBots/KaurSardarniMusic",
+    "https://github.com/MrAujla/YukkiMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
